654aaf974aba1cef0f227b13ec899f4dd6d4bebd
jcm2606 <jakemichie97@hotmail.com>
2025-08-02 22:15:28 +1000

    More work and a license.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,35 @@
 use crate::noisetex::{Noisetex, NoisetexRgba8};
 use crate::samplers::perlin::sample_perlin_fbm;
+use crate::samplers::worley::sample_worley_fbm3;
+use crate::util::{remap, remap_clamp};
 
 mod noisetex;
 mod samplers;
 mod util;
 
 fn main() {
-    let mut noisetex = NoisetexRgba8::new(64, 64, 1);
+    let mut noisetex = NoisetexRgba8::new(128, 128, 1);
 
     noisetex.fill(|uvw, pixel| {
-        let perlin = sample_perlin_fbm(uvw, 8f32, 3);
-        let perlin = (perlin * 255f32) as u8;
+        let mut seed = 4f32;
 
-        pixel.r = perlin;
-        pixel.g = perlin;
-        pixel.b = perlin;
+        let perlin_worley = {
+            let perlin_fbm = sample_perlin_fbm(uvw, 8f32, 3, seed);
+
+            seed *= 2f32;
+            let worley_fbm = sample_worley_fbm3(uvw, 6f32, seed);
+
+            remap_clamp(perlin_fbm, 0f32, 1f32, worley_fbm, 1f32)
+        };
+        let perlin_worley = (perlin_worley * 255f32) as u8;
+
+        pixel.r = perlin_worley;
+        pixel.g = perlin_worley;
+        pixel.b = perlin_worley;
         pixel.a = 255;
     });
 
-    noisetex.save_as_image("output/test.png");
+    noisetex.save_as_image("output/lfCloudNoiseTex.png");
 
     println!("Fin!");
 }
--- src/samplers/perlin.rs
+++ src/samplers/perlin.rs
@@ -1,58 +1,63 @@
 use glam::Vec3;
 
 use crate::samplers::hash33;
-use crate::util::clamp;
+use crate::util::{clamp, mix};
 
-fn project(p: glam::Vec3, f: glam::Vec3, offset: glam::Vec3, frequency: f32) -> f32 {
-    let gradient = hash33((p + offset).rem_euclid(Vec3::splat(frequency))) * 2.0f32 - 1.0f32;
+fn project(p: glam::Vec3, f: glam::Vec3, offset: glam::Vec3, frequency: f32, seed: f32) -> f32 {
+    let gradient = hash33((p + offset + seed).rem_euclid(Vec3::splat(frequency))) * 2.0f32 - 1.0f32;
     Vec3::dot(gradient, f - offset)
 }
 
 /// outputs [-1, 1)
-pub fn sample_perlin_noise(uvw: glam::Vec3, frequency: f32) -> f32 {
+pub fn sample_perlin_noise(uvw: glam::Vec3, frequency: f32, seed: f32) -> f32 {
     let p = uvw.floor();
     let f = uvw.fract();
 
     let u = f * f * f * (f * (f * 6f32 - 15f32) + 10f32);
     // let u = f * f * (3f32 - 2f32 * f);
 
-    let projection_a = project(p, f, glam::vec3(0f32, 0f32, 0f32), frequency);
-    let projection_b = project(p, f, glam::vec3(1f32, 0f32, 0f32), frequency);
-    let projection_c = project(p, f, glam::vec3(0f32, 1f32, 0f32), frequency);
-    let projection_d = project(p, f, glam::vec3(1f32, 1f32, 0f32), frequency);
-    let projection_e = project(p, f, glam::vec3(0f32, 0f32, 1f32), frequency);
-    let projection_f = project(p, f, glam::vec3(1f32, 0f32, 1f32), frequency);
-    let projection_g = project(p, f, glam::vec3(0f32, 1f32, 1f32), frequency);
-    let projection_h = project(p, f, glam::vec3(1f32, 1f32, 1f32), frequency);
+    let projection_a = project(p, f, glam::vec3(0f32, 0f32, 0f32), frequency, seed);
+    let projection_b = project(p, f, glam::vec3(1f32, 0f32, 0f32), frequency, seed);
+    let projection_c = project(p, f, glam::vec3(0f32, 0f32, 1f32), frequency, seed);
+    let projection_d = project(p, f, glam::vec3(1f32, 0f32, 1f32), frequency, seed);
+    let projection_e = project(p, f, glam::vec3(0f32, 1f32, 0f32), frequency, seed);
+    let projection_f = project(p, f, glam::vec3(1f32, 1f32, 0f32), frequency, seed);
+    let projection_g = project(p, f, glam::vec3(0f32, 1f32, 1f32), frequency, seed);
+    let projection_h = project(p, f, glam::vec3(1f32, 1f32, 1f32), frequency, seed);
 
-    projection_a
-        + u.x * (projection_b - projection_a)
-        + u.y * (projection_c - projection_a)
-        + u.z * (projection_e - projection_a)
-        + u.x * u.y * (projection_a - projection_b - projection_c + projection_d)
-        + u.y * u.z * (projection_a - projection_c - projection_e + projection_g)
-        + u.z * u.x * (projection_a - projection_b - projection_e + projection_f)
-        + u.x
-            * u.y
-            * u.z
-            * (-projection_a + projection_b + projection_c - projection_d + projection_e
-                - projection_f
-                - projection_g
-                + projection_h)
+    mix(
+        mix(
+            mix(projection_a, projection_b, u.x),
+            mix(projection_c, projection_d, u.x),
+            u.z,
+        ),
+        mix(
+            mix(projection_e, projection_f, u.x),
+            mix(projection_g, projection_h, u.x),
+            u.z,
+        ),
+        u.y,
+    )
 }
 
-pub fn sample_perlin_fbm(uvw: glam::Vec3, mut frequency: f32, num_octaves: usize) -> f32 {
+pub fn sample_perlin_fbm(
+    uvw: glam::Vec3,
+    mut frequency: f32,
+    num_octaves: usize,
+    mut seed: f32,
+) -> f32 {
     let mut weight = 1.0f32;
 
     let mut sum = 0f32;
     let mut weight_sum = 0f32;
 
     for octave in 0..num_octaves {
-        sum += (sample_perlin_noise(uvw * frequency, frequency)) * weight;
+        sum += (sample_perlin_noise(uvw * frequency, frequency, seed)) * weight;
         weight_sum += weight;
 
         weight *= 0.5;
         frequency *= 2.0;
+        seed += 10f32;
     }
 
     clamp((sum / weight_sum) * 0.5f32 + 0.5f32, 0f32, 1f32)
